c80be29f8db6d252cd605950e8ec0d3cf847a0cc
mtafar <mtanus_messi@hotmail.com>
2019-11-18 10:27:45 +0100

    first Commit

---
+++ StrategyPattern/src/sample/Main.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        BorderPane root = new BorderPane();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
